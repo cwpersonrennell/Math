4e09740de5adedb470819182362723fd0e81c656
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2022-10-19 20:30:00 -0700

    Update dynamicLoad.js

--- dynamicLoad.js
+++ dynamicLoad.js
@@ -4,17 +4,24 @@ function renderPage(){
 }
       
 function loadHTML(href,target,container='div'){
-  fetch(href).then( response=>{return response.text();}).then(content=>{
+  promise = fetch(href).then( response=>{return response.text();}).then(content=>{
     let shell = document.createElement(container);
     shell.innerHTML=content;
     target.appendChild(shell);
    // renderPage();
   });
+  return promise
 }
 const HTML_ROOT = "https://cwpersonrennell.github.io/Math/";
 
 function loadFiles(files,target,container='div'){
+  let promises = [];
   for(let i = 0;i<files.length;i++){
-    loadHTML(`${HTML_ROOT}${files[i]}.html`,target,container);
+    let promise=loadHTML(`${HTML_ROOT}${files[i]}.html`,target,container);
+    promises.push(promise);
   }
+  Promise.all(promises).then(()=>{
+    console.log("All elements fetched and embedded, rendering page");
+    renderPage();
+  });
 }
