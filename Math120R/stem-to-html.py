e925e239047f2471155888a88072c950b46b73c3
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-02-02 14:21:45 -0700

    adding automation features

---
+++ Math120R/stem-to-html.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on February 2nd 2023
+
+@author: cpers
+"""
+
+from pathlib import Path
+import os
+
+path=Path(".")
+Stem_filepaths = list(path.glob("Stems/*/*.html"))
+Stems=list(path.glob("Stems/*"))
+for i in range(0,len(Stems)):
+    pathname= os.fspath(Stems[i].absolute()).replace("Stems\\","")
+
+    try:
+        os.mkdir(pathname)
+    except OSError as e:
+        print(e)
+
+Chapter_filepaths = []
+
+template_file = open("main-body-template.html")
+
+for i in range(0,len(Stem_filepaths)):
+    pathname= os.fspath(Stem_filepaths[i].absolute()).replace("Stems\\","")
+    body_contents = open(Stem_filepaths[i]).read()
+    print(pathname)
+    newFile = open(pathname,'w')
+    newFile.write(template_file.read().replace("{{{body}}}",body_contents).replace("{{{title}}}",Stem_filepaths[i].name.split(".html")[0]))
+    newFile.close()
+
