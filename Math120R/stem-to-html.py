38e56c48b5e2e8ce68d7bd50a96913738a9e87ca
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-02-02 14:42:42 -0700

    working on automation

--- Math120R/stem-to-html.py
+++ Math120R/stem-to-html.py
@@ -26,8 +26,16 @@ template_file = open("main-body-template.html")
 for i in range(0,len(Stem_filepaths)):
     pathname= os.fspath(Stem_filepaths[i].absolute()).replace("Stems\\","")
     body_contents = open(Stem_filepaths[i]).read()
-    print(pathname)
+    template_file = open("main-body-template.html")
+    new_contents = template_file.read().replace("{{{body}}}",body_contents).replace("{{{title}}}",Stem_filepaths[i].name.split(".html")[0])
+    try:
+        originalFile=open(pathname,"w")
+        if originalFile.read() == new_contents:
+            continue
+    except:
+        pass
+    
     newFile = open(pathname,'w')
-    newFile.write(template_file.read().replace("{{{body}}}",body_contents).replace("{{{title}}}",Stem_filepaths[i].name.split(".html")[0]))
+    newFile.write(new_contents)
     newFile.close()
 
