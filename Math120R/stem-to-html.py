ba91b45da95fb2f7a022bc4d04465c981cb192a5
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-04-15 21:26:54 -0700

    Added D2L Page Support
    Automated creation of D2L page for ease of uploading to course content. D2L template creates an iframe to embed in the d2l page with the relevant link baked in as well as some basic formatting to allow for it to appear nicely in the d2l environment

--- Math120R/stem-to-html.py
+++ Math120R/stem-to-html.py
@@ -19,13 +19,18 @@ def removeDoubleBraces(string):
 path=Path(".")
 Stem_filepaths = list(path.glob("Stems/*/*.html"))
 Stem_filepaths.extend(list(path.glob("Stems/*/*/*.html")))
-Stems=list(path.glob("Stems/*"))
+Stems=list(path.glob("Stems/*[!.html]/"))
+Stems.extend(list(path.glob("Stems/*/*[!.html]/")))
 
 for i in range(0,len(Stems)):
     pathname= os.fspath(Stems[i].absolute()).replace("Stems\\","")
-
+    D2Lpathname= os.fspath(Stems[i].absolute()).replace("Stems","D2LPages")
     try:
         os.mkdir(pathname)
+    except OSError as e:
+        print(e)
+    try:
+        os.mkdir(D2Lpathname)
     except OSError as e:
         print(e)
 
@@ -36,20 +41,36 @@ template_file = open("main-body-template.html")
 def makeHTMLFilesFromStems():
     for i in range(0,len(Stem_filepaths)):
         pathname= os.fspath(Stem_filepaths[i].absolute()).replace("Stems\\","")
+        D2Lpathname= os.fspath(Stem_filepaths[i].absolute()).replace("Stems","D2LPages")
         body_contents = removeDoubleBraces(open(Stem_filepaths[i]).read())
+        D2Lsrc=D2Lpathname.split("D2LPages\\")[1]
         
         template_file = open("main-body-template.html")
         new_contents = template_file.read().replace("{{{body}}}",body_contents).replace("{{{title}}}",Stem_filepaths[i].name.split(".html")[0])
         try:
-            originalFile=open(pathname,"w")
-            if originalFile.read() == new_contents:
-                continue
+            originalFile=open(pathname,"r")
+            if originalFile.read() != new_contents:
+                newFile = open(pathname,'w')
+                newFile.write(new_contents)
+                newFile.close()  
+        except:
+            pass
+        originalFile.close()
+        template_file.close()
+        
+        template_file = open("D2L-template.html")
+        new_contents = template_file.read().replace("{{{src}}}",D2Lsrc)
+        try:
+            originalFile = open(D2Lpathname,'r')
+            if originalFile.read() != new_contents:
+                print("Writing D2L Page...")
+                newFile = open(D2Lpathname,'w')
+                newFile.write(new_contents)
+                newFile.close()
         except:
             pass
         
-        newFile = open(pathname,'w')
-        newFile.write(new_contents)
-        newFile.close()
-
-
+        originalFile.close()
+        template_file.close()
+        
 makeHTMLFilesFromStems()
