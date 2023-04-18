70c0f395b3cd04d7bb117cb44e3c22fdd0b0af2e
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2023-04-18 15:05:13 -0700

    Fixed the HTML to Stem function for better readability and ease of use

--- Math120R/stem-to-html.py
+++ Math120R/stem-to-html.py
@@ -39,42 +39,40 @@ for i in range(0,len(Stems)):
 
 template_file = open("main-body-template.html")
 
+def CreateAndWriteContentsToFile(content,filename):
+    try:
+        newFile = open(filename,'w')
+        originalFile = open(filename,'r')
+        if originalFile.read!=content:
+            newFile.write(content)
+        newFile.close()
+        originalFile.close()
+    except:
+        print("Error Creating/Writing the File: ",filename)
+            
 
 def makeHTMLFilesFromStems():
     for i in range(0,len(Stem_filepaths)):
-        print(Stem_filepaths[i].absolute())
+        #Collect the filename and rename them to the new locations
         pathname= os.fspath(Stem_filepaths[i].absolute()).replace("Stems\\","")
         D2Lpathname= os.fspath(Stem_filepaths[i].absolute()).replace("Stems","D2LPages")
+        
+        #Some older files from D2L doubled braces unneccessarily, this removes them.
         body_contents = removeDoubleBraces(open(Stem_filepaths[i]).read())
+        
+        #Create the appropriate href for the link in the D2L stub.
         D2Lsrc=D2Lpathname.split("D2LPages\\")[1]
         
+        #Write the actual Source Html File        
         template_file = open("main-body-template.html")
         new_contents = template_file.read().replace("{{{body}}}",body_contents).replace("{{{title}}}",Stem_filepaths[i].name.split(".html")[0])
-        try:
-            newFile = open(pathname,'w')            
-            originalFile=open(pathname,"r")
-            if originalFile.read() != new_contents:
-                newFile.write(new_contents)
-            newFile.close()  
-            originalFile.close()
-        except:
-            print("File Error")
-            
-            pass
-
+        CreateAndWriteContentsToFile(new_contents,pathname)
+        
+        #Write the HTML stub for D2L integration (copy paste into D2L new file for link to HTML source)
         D2Ltemplate_file = open("D2L-template.html")
         new_contents = D2Ltemplate_file.read().replace("{{{src}}}",D2Lsrc)
         D2Ltemplate_file.close()
-        try:
-            newFile = open(D2Lpathname,'w')            
-            originalFile=open(D2Lpathname,"r")
-            if originalFile.read() != new_contents:
-                newFile.write(new_contents)
-            newFile.close()  
-            originalFile.close()
-        except:
-            pass
-        
+        CreateAndWriteContentsToFile(new_contents,D2Lpathname)
         
         
 makeHTMLFilesFromStems()
