cb44693dad32d2dc73a0a84e0fe9e238759c1b8c
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-15 12:41:25 -0700

    more testing

--- Interact/main.js
+++ Interact/main.js
@@ -6,16 +6,24 @@ var jsonEl = document.getElementById("json-content");
 var previewEl = document.getElementById("preview");
 
 button.addEventListener("click",()=>{
+	let name=document.getElementById("name").value;
 	let code=document.getElementById("code").value;
 	let vars=document.getElementById("vars").value.replaceAll(" ","").split(",");
 	let body=document.getElementById("body").value;
-	let data = {"vars":vars,"code":code,"body":body};
+	let data = {"name":name,"vars":vars,"code":code,"body":body};
 	let json = JSON.stringify(data)
 	jsonEl.value = json;
 
 	let output = sandbox(vars,code);
 	console.log(output);
-	
+	for(let i = 0;i<vars.length;i++){
+		body = body.replaceAll(`{{${vars[i]}}}`,`${output[vars[i]]}`);
+	}
+	previewEl.innerHTML = body;
+	try{
+		MathJax.typeset();
+	}catch(err){console.log(err);}
+
 });
 
 
