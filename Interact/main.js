f85d3ecc408eeceb3b759d78c3b57f7dd2fbb9d6
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-15 11:59:20 -0700

    Added Sandbox module after testing

--- Interact/main.js
+++ Interact/main.js
@@ -1,38 +1,5 @@
 import {polynomial} from "./modules/Polynomial.js";
-import data from "./database/testing.json" with {type:"json"};
+import {sandbox} from "./modules/Sandbox.js";
 
-function initializeVars(_vars_){
-	let result = `var _exports_ = {};\n`;
-	for(let i =0;i<_vars_.length;i++){
-		result+=`var ${_vars_[i]} = '';\n`;
-		result+=`_exports_.${_vars_[i]}='';\n`;
-	}
-	return result;
-}
-function readyExport(_vars_){
-	let result = `\n`;
-	for(let i =0;i<_vars_.length;i++){
-		result+=`_exports_.${_vars_[i]}=${_vars_[i]};\n`;
-	}
-	result+=`\n_exports_;\n`;
-	return result;
-}
 
-function sandbox(_vars_,code){
-	let result = eval?.(
-		`
-		"use strict";
-		${initializeVars(_vars_)}
-		${code}
-		${readyExport(_vars_)}
-		`);
-	return result;
-}
-console.log(data);
-var _exports_;
-try{
-	_exports_ = sandbox(data["vars"],data["code"]);
-}
-catch(err){console.log(err);}
 
-console.log(_exports_);
