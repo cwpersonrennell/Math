eaede0b0fcaefa153a94b1a9104e73146cccd8d4
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-15 12:19:11 -0700

    Update main.js

--- Interact/main.js
+++ Interact/main.js
@@ -1,16 +1,12 @@
 import {polynomial} from "./modules/Polynomial.js";
 import {sandbox} from "./modules/Sandbox.js";
 
-var code=document.getElementById("code").value;
-var vars=document.getElementById("vars").value.replaceAll(" ","").split(",");
 let button = document.getElementById("go");
-
 button.addEventListener("click",()=>{
-	var data = {"vars":vars,"code":code};
-	var json = JSON.stringify(data)
-	console.log(code);
-	console.log(vars);
-	console.log(json);
+	let code=document.getElementById("code").value;
+	let vars=document.getElementById("vars").value.replaceAll(" ","").split(",");
+	let data = {"vars":vars,"code":code};
+	let json = JSON.stringify(data)
 	document.getElementById("content").innerText = json;	
 });
 
