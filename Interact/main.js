38e7f056d7bfbb6eecd4e8e29850aaea9c037022
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-15 10:04:11 -0700

    Testing Eval Features

--- Interact/main.js
+++ Interact/main.js
@@ -3,3 +3,10 @@ import {polynomial} from "./modules/Polynomial.js";
 let p1 = new polynomial([1,1,-1]);
 
 console.log(`${p1}`);
+var x = 0;
+eval("x++;");
+
+eval(`
+	f = function(x){return x*x}
+	`);
+console.log(f(2));
