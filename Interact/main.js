9077476cc0c589c065261082ea39efa862d801d9
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-15 13:00:16 -0700

    Adding Save/Load to localStorage

--- Interact/main.js
+++ Interact/main.js
@@ -1,15 +1,49 @@
 import {polynomial} from "./modules/Polynomial.js";
 import {sandbox} from "./modules/Sandbox.js";
 
-var button = document.getElementById("go");
+
+
+function loadLocalStorageList(loadEl){
+	let keys = Object.keys(localStorage);
+	let results = [];
+	let database = {};
+	for(let i = 0;i<keys.length;i++){
+		if(keys.search(location.href)>=0){
+			results.push(JSON.parse(localStorage[key]));
+		}
+	}
+	
+	for(let i=0;i<results.length;i++){
+		let el = document.createElement
+		el.innerText = results[i].name;
+		el.value = results[i].name;
+		database[name] = results[i];
+		loadEl.appendChild(el);
+	}
+	return database;
+}
+var loadEl = document.getElementById("load");
+var database = loadLocalStorageList(loadEl);
+
+loadEl.addEventListener("change",function(){
+	let name = loadEl.value;
+	console.log(name);
+})
+
+var save = document.getElementById("save");
+
 var jsonEl = document.getElementById("json-content");
 var previewEl = document.getElementById("preview");
+var nameEl = document.getElementById("name");
+var codeEl = document.getElementById("code");
+var varsEl = document.getElementById("vars");
+var bodyEl =document.getElementById("body");
 
-button.addEventListener("click",()=>{
-	let name=document.getElementById("name").value;
-	let code=document.getElementById("code").value;
-	let vars=document.getElementById("vars").value.replaceAll(" ","").split(",");
-	let body=document.getElementById("body").value;
+save.addEventListener("click",()=>{
+	let name=nameEl.value;
+	let code=codeEl.value;
+	let vars=varsEl.value.replaceAll(" ","").split(",");
+	let body=bodyEl.value;
 	let data = {"name":name,"vars":vars,"code":code,"body":body};
 	let json = JSON.stringify(data)
 	jsonEl.value = json;
@@ -22,6 +56,7 @@ button.addEventListener("click",()=>{
 	previewEl.innerHTML = body;
 	try{
 		MathJax.typeset();
+		localStorage[`${location.href}:${name}`]=json;
 	}catch(err){console.log(err);}
 
 });
