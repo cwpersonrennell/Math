79e9c675047a31aeece28bd3bc04e42a2a79ced5
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-15 12:17:21 -0700

    Update main.js

--- Interact/main.js
+++ Interact/main.js
@@ -3,10 +3,11 @@ import {sandbox} from "./modules/Sandbox.js";
 
 var code=document.getElementById("code").value;
 var vars=document.getElementById("vars").value.replaceAll(" ","").split(",");
-var json = JSON.stringify({"vars":vars,"code":code});
 let button = document.getElementById("go");
 
 button.addEventListener("click",()=>{
+	var data = {"vars":vars,"code":code};
+	var json = JSON.stringify(data)
 	console.log(code);
 	console.log(vars);
 	console.log(json);
