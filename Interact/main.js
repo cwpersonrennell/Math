93f927635cee3eb5c16bd257c2425085aede775a
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2024-10-15 12:14:43 -0700

    Update main.js

--- Interact/main.js
+++ Interact/main.js
@@ -5,7 +5,11 @@ var code=document.getElementById("code").value;
 var vars=document.getElementById("vars").value.replaceAll(" ","").split(",");
 var json = JSON.stringify({"vars":vars,"code":code});
 let button = document.getElementById("go");
+
 button.addEventListener("click",()=>{
+	console.log(code);
+	console.log(vars);
+	console.log(json);
 	document.getElementById("content").innerText = json;	
 });
 
